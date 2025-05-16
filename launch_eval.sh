7a8c71de63b5f461b4400a98d434f228a6145a7d
Monte Hoover <monte.b.hoover@gmail.com>
2025-05-15 22:56:39 -0400

    Update eval script

--- launch_eval.sh
+++ launch_eval.sh
@@ -1,97 +1,25 @@
 # Run with:
 # launch launch_eval.sh --classical_logfile_names --gpu_type rtxa5000 --mem 30
 
-# # Our Test Set
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model Qwen/Qwen2.5-0.5B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_wildguard  
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model gpt-4o
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model Qwen/Qwen3-0.6B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model Qwen/Qwen3-1.7B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model Qwen/Qwen3-4B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model Qwen/Qwen3-8B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model Qwen/Qwen3-14B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model meta-llama/Llama-Guard-3-8B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model nvidia/llama-3.1-nemoguard-8b-content-safety
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model tomg-group-umd/qwen3_8B_compliance_cot_mix_80k_lr1e-5_epoch1_bs128
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset compliance --split test --model tomg-group-umd/qwen3_8B_comp_cot_mix_80k_lr1e-5_epoch1_bs128_32k_mix_grpo_lr1e-6_bs48-epochs1_3k_maxlen1024
 
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model Qwen/Qwen2.5-1.5B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_wildguard
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model gpt-4o
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model Qwen/Qwen3-0.6B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model Qwen/Qwen3-1.7B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model Qwen/Qwen3-4B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model Qwen/Qwen3-8B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model Qwen/Qwen3-14B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model meta-llama/Llama-Guard-3-8B
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model nvidia/llama-3.1-nemoguard-8b-content-safety
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model tomg-group-umd/qwen3_8B_compliance_cot_mix_80k_lr1e-5_epoch1_bs128
+python scripts/eval.py --sample_size 4 --dataset_path montehoover/compliance --subset wildguard --split test_harm --model tomg-group-umd/qwen3_8B_comp_cot_mix_80k_lr1e-5_epoch1_bs128_32k_mix_grpo_lr1e-6_bs48-epochs1_3k_maxlen1024
 
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model Qwen/Qwen2.5-3B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model Qwen/Qwen2.5-7B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model Qwen/Qwen2.5-14B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model meta-llama/Llama-Guard-3-8B
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Llama-Guard-3-8B/huggingface_sft/lora_multirule_v2
-
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model meta-llama/meta-Llama-3.1-8B-Instruct
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Meta-Llama-3.1-8B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model /fs/cml-projects/guardian_models/models/Meta-Llama-3.1-8B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model gpt-4o-mini
-python scripts/eval.py --sample_size 10 --dataset_path tomg-group-umd/compliance --subset compliance --split test --model gpt-4o
-
-
-# XSTest
-python scripts/eval_xstest.py --sample_size  5 --model Qwen/Qwen2.5-0.5B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model Qwen/Qwen2.5-1.5B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model Qwen/Qwen2.5-3B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model Qwen/Qwen2.5-7B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model Qwen/Qwen2.5-14B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model meta-llama/Llama-Guard-3-8B
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Llama-Guard-3-8B/huggingface_sft/lora_multirule_v2
-
-python scripts/eval_xstest.py --sample_size  5 --model meta-llama/meta-Llama-3.1-8B-Instruct
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Meta-Llama-3.1-8B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval_xstest.py --sample_size  5 --model /fs/cml-projects/guardian_models/models/Meta-Llama-3.1-8B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval_xstest.py --sample_size  5 --model gpt-4o-mini
-python scripts/eval_xstest.py --sample_size  5 --model gpt-4o
-
-
-# WildGuard
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model Qwen/Qwen2.5-0.5B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model Qwen/Qwen2.5-1.5B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-1.5B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model Qwen/Qwen2.5-3B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-3B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model Qwen/Qwen2.5-7B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-7B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model Qwen/Qwen2.5-14B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-14B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model meta-llama/Llama-Guard-3-8B
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Llama-Guard-3-8B/huggingface_sft/lora_multirule_v2
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model meta-llama/meta-Llama-3.1-8B-Instruct
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_multirule_v2
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model /fs/cml-projects/guardian_models/models/Qwen2.5-0.5B-Instruct/huggingface_grpo/lora_wildguard
-
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model gpt-4o-mini
-python scripts/eval.py --sample_size 1 --dataset_path tomg-group-umd/compliance --subset wildguard  --model gpt-4o
